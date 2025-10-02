159d41988b65718a4867822a865ef41db8de24f4
HrachGyulamiryan <hrachgyulamiryan@gmail.com>
2025-10-02 16:02:59 +0400

    Media_Image

--- src/components/Media/index.tsx
+++ src/components/Media/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 const images = [1, 2, 3, 4, 5];
+import Image from "next/image";
 
 export default function Media() {
 
@@ -12,34 +13,34 @@ export default function Media() {
   ]
 
   return (
-    // relative z-20 overflow-hidden bg-white pb-8 pt-20 dark:bg-dark lg:pb-[30px] lg:pt-[200px]
     <div className="relative min-h-screen pt-20 pb-8 bg-gray-100 dark:bg-gray-900 p-6 lg:pt-[200px]">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pb-6">
         {images.map((image, index) => (
-          <div
-            key={`img-${index}`}
-            className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md flex items-center justify-center max-h-[250px]"
+          <div 
+            key={index} 
+            className="relative w-full h-[300px] rounded-md overflow-hidden"
           >
-            <img
-              src={`/images/about/${image}.jpg`}
-              alt={String(image)}
-              className="max-w-full max-h-full rounded-md object-cover"
+            <Image
+              src={`/images/about...../${image}.jpg`}
+              alt={`Image ${image}`}
+              fill
+              style={{ objectFit: "cover", objectPosition: "100% 80%" }}
+              className="rounded-md" 
+              priority={index === 0}
             />
           </div>
         ))}
       </div>
-
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {videos.map((video, index) => (
           <div
             key={`vid-${index}`}
-            className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md"
           >
             <video 
               controls
-              className="w-full h-auto rounded-md min-h-[250px]"
+              className="w-full h-auto rounded-md min-h-[300px]"
             >
-              <source src={video.src} type="video/webm" />
+              <source src={video.src} type="video/mp4" />
             </video>
           </div>
         ))}
