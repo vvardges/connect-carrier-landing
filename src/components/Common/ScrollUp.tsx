933dbb238e8b9123b2c1f84bb49322210c933fe3
HrachGyulamiryan <hrachgyulamiryan@gmail.com>
2025-11-12 12:27:39 +0400

    LintFix

--- src/components/Common/ScrollUp.tsx
+++ src/components/Common/ScrollUp.tsx
@@ -1,10 +1,12 @@
 "use client";
-
 import { useEffect } from "react";
 
 export default function ScrollUp() {
-  if (typeof window === "undefined") return;
-  useEffect(() => window.document.scrollingElement?.scrollTo(0, 0), []);
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    window.document.scrollingElement?.scrollTo(0, 0);
+  }, []);
 
   return null;
 }
